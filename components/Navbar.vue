d3ecd5334dbafc18d4f92762819c8ded33e38a5f
shubhangchourasia <shubhangc99@gmail.com>
2020-09-04 12:07:37 +0530

    Test

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,51 +1,56 @@
 <template>
-    <div>
-        <!-- <nuxt-link to='/'>Home</nuxt-link>
-        <nuxt-link to='/hr/hr'>HR</nuxt-link> -->
-     <nav class="navbar navbar-expand-md  navbar-light">
-            <!-- Brand -->
-            <h2 class="col-2 logo">LOGO</h2>
-          
-            <!-- Toggler/collapsibe Button -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          
-            <!-- Navbar links -->
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <nuxt-link to='/' class="nav-link">Home</nuxt-link>
-                </li>
-                 <li class="nav-item">
-                      <nuxt-link to='' class="nav-link">Product</nuxt-link>
-                <li class="nav-item">
-                  <!-- <a class="nav-link" href="#">INTERNSHIP</a> -->
-                  <nuxt-link to='' class="nav-link">Login</nuxt-link>
-                </li>
-                <li class="nav-item">
-                 <nuxt-link to='' class="nav-link">Signup</nuxt-link>
-                 <li class="nav-item">
-                  <nuxt-link to='' class="nav-link">Contact</nuxt-link>    
-                </li>
-              </ul>
-            </div>
-          </nav>
+  <div class="row">
+    <div class="col-12">
+      <!-- <nuxt-link to='/'>Home</nuxt-link>
+      <nuxt-link to='/hr/hr'>HR</nuxt-link>-->
+      <nav class="navbar navbar-expand-md navbar-light">
+        <!-- Brand -->
+        <h2 class="col">LOGO</h2>
 
+        <!-- Toggler/collapsibe Button -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <nuxt-link to="/" class="nav-link">Home</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to class="nav-link">Product</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <!-- <a class="nav-link" href="#">INTERNSHIP</a> -->
+              <nuxt-link to class="nav-link">Login</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to class="nav-link">Signup</nuxt-link>
+            </li>
+            <li class="nav-item">
+              <nuxt-link to class="nav-link">Contact</nuxt-link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {}
 </script>
 
 <style scoped>
-.logo{    
-    border-right:5px solid #f1f3f5;
-}
-.navbar{
-    border-bottom: 5px solid #f1f3f5;
+.navbar {
+  border-bottom: 5px solid #f1f3f5;
+  position: fixed;
+  top: 0;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,16 @@
 <template>
   <div>
-    <Navbar/>
-    <Nuxt />
+    <Navbar class="mb-5" />
+    <!-- <Sidebar/> -->
+    <hr />
+    <Nuxt class="mt-5" />
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
