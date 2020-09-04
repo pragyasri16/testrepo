7c56825c504ac845e982653ca0f4e4bdd663a1db
pragyasri16 <pragyatekumalla1616@gmail.com>
2020-09-04 12:39:23 +0530

    new

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <Navbar/>
+    <Navbar />
+    <Signin />
     <Nuxt />
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
---
+++ layouts/signin.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <h2>Login Form</h2>
+    </div>
+
+    <form>
+      <div class="container">
+        <label for="uname">
+          <b>Username</b>
+        </label>
+        <input type="text" placeholder="Enter Username" name="uname" required />
+
+        <label for="psw">
+          <b>Password</b>
+        </label>
+        <input type="password" placeholder="Enter Password" name="psw" required />
+
+        <button type="submit">Login</button>
+        <label>
+          <input type="checkbox" checked="checked" name="remember" /> Remember me
+        </label>
+      </div>
+
+      <div class="container" style="background-color: #f1f1f1">
+        <button type="button" class="cancelbtn">Cancel</button>
+        <span class="psw">
+          Forgot
+          <a href="#">password?</a>
+        </span>
+      </div>
+    </form>
+  </div>
+  <!-- <template> </template> -->
+</template>
