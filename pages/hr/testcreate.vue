1ebc7dc84c06fd160af8a2f924a5044dce009d5e
chandankawale18 <chandankawale6@gmail.com>
2020-09-15 12:12:42 +0530

    newUpdate

--- pages/hr/testcreate.vue
+++ pages/hr/testcreate.vue
@@ -1,163 +1,136 @@
 <template>
-    <div style="background: #f1f3f5">
-        <div class="container-sm" id="container">
-           <h2>Create a new Smart Test</h2>
-           <hr id="hline1"> 
-
-            <form action class="main-form">
-                <div class="row">
+<div class="container">
+  <h1>Create a new Smart Test</h1>
+    <form action class="main-form-sm-md" id="form1">
+        <div class="row">
                     <div class="col-3">
                         <div class="form-group">
-                            <label for="jobtitle" id="jobname">Job Title:</label>
+                            <label for="jobtitle" id="jobname">Job Title :</label>
                         </div>
                     </div>
+
                     <div class="col-3">
                         <input type="text" name="jobtitle" id="jobtitle" class="form-control" />
                     </div>
                     
                     <div class="col-3">
                         <div class="form-group">
-                            <label for="jobtitle" id="timename">Time Duration:</label>
+                            <label for="jobtitle" id="timename">Time Duration :</label>
                         </div>
                     </div>
+
                     <select class="custom-select">
                         <option selected>15 Minutes</option>
                         <option value="1">20 Minutes</option>
                         <option value="2">25 Minutes</option>
                         <option value="3">30 Minutes</option>
                     </select>
-                </div>
 
-                <div class="col-3">
-                    <div class="form-group">
-                        <label for="jobtitle" id="skillname">Choose Skills for Test:</label>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label for="jobtitle" id="skillname">Choose Skills for Test :</label>
+                        </div>
                     </div>
-                </div>
-                <br>
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                        <div class="btn-group" role="group" aria-label="1">
-                            <!-- <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">1</button>
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">2</button>
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">3</button>
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">4</button>
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">5</button>
-                             -->
-                        </div>
-                        <div class="btn-group" role="group" aria-label="Third group">
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample">All Sector</button>
+
+                    <div class="col-3">
+                        <div class="menu">
+                            <p id="hmenu1" v-on:click="cardShow">All-Sector</P> 
+                            <p id="hmenu2" v-on:click="cardShow2">IT-Sector</P>
+                            <p id="hmenu3" v-on:click="cardShow3">Apptitude</P>
+                            <p id="hmenu4" v-on:click="cardShow4">English</P>
+                            <p id="hmenu5" v-on:click="cardShow5">Reasoning</P>  
                         </div>
 
-                        <div class="btn-group" role="group" aria-label="Third group">
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample">IT Sector</button>
+                        <div class="container-fluid">
+                                <div class="card" v-if="contView">
+                                    <div class="card-body bg-primary text-white">Basic card 1</div>
+                                </div>
+                                <div class="card" v-if="contView2">
+                                    <div class="card-body bg-primary text-white">Basic card 2</div>
+                                </div>
+                                <div class="card" v-if="contView3">
+                                    <div class="card-body bg-primary text-white">Basic card 3</div>
+                                </div>
+                                <div class="card" v-if="contView4">
+                                    <div class="card-body bg-primary text-white">Basic card 4</div>
+                                </div>
+                                <div class="card" v-if="contView5">
+                                    <div class="card-body bg-primary text-white">Basic card 5</div>
+                                </div>
                         </div>
-
-                        <div class="btn-group" role="group" aria-label="Third group">
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample"></button>
-                        </div>
-
-                        <div class="btn-group" role="group" aria-label="Third group">
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample">8</button>
-                        </div>
-
-                        <div class="btn-group" role="group" aria-label="Third group">
-                            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#collapseExample">8</button>
-                        </div>
-                        
                     </div>
                     
-                    <div class="collapse" id="collapseExample">
-                        <div class="card card-body">
-                            <div class="form-check">
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" value="">Option 1
-                                </label>
-
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" value="">Option 1
-                                </label>
-
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" value="">Option 1
-                                </label>
-
-                                <label class="form-check-label">
-                                    <input type="checkbox" class="form-check-input" value="">Option 1
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- <div class="dropdown">
-                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" @click="toggleDropdown($event)", data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Choose Skills
-                        </a>
-
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <a class="dropdown-item" href="#">All Sector</a>
-                            <a class="dropdown-item" href="#">IT Sector</a>
-                            <a class="dropdown-item" href="#">Apptitude</a>
-                            <a class="dropdown-item" href="#">English</a>
-                        </div>
-                            
-                    </div> -->
-                
-                      
-            </form>
+                    
+        </div>
                     <button type="button" class="btn1">Archieve</button>
-                    <button type="button" class="btn1">Custom Test</button>
-                    <button type="button" class="btn1">Reset</button>
-                    <button type="button" class="btn1">Genrate Test</button>
-        </div>      
-    </div>
+                    <button type="button" class="btn2">Custom Test</button>
+                    <button type="button" class="btn3">Reset</button>
+                    <button type="button" class="btn4">Genrate Test</button>
+    </form>
+</div>
 </template>
 
 <script>
     export default {
-        layout:'hr'
-        
-    // methods: {
-    //         toggleDropdown(event) {
-    //             event.currentTarget.classList.toggle('is-active')
-    //         }
-    //     }
- }
+        layout:'hr',
+
+        data(){
+            return{
+                contView: false,
+                contView2: false,
+                contView3: false,
+                contView4: false,
+                contView5: false
+            };
+        },
+
+        methods:{
+                   cardShow(){
+                           this.contView = true;
+                   },
+                   cardShow2(){
+                           this.contView2 = true;
+                   },
+                   cardShow3(){
+                           this.contView3 = true;
+                   },
+                   cardShow4(){
+                           this.contView4 = true;
+                   },
+                   cardShow5(){
+                           this.contView5 = true;
+                   }
+                }
+        };
+ 
 </script>
 
 <style scoped>
-#container{
-    width: 1080px;
-    height: 500px;
-    margin-left: 260px;
-    margin-top: 50px;
-    /* margin-top: px; */
-    float: center;
-    background: #ffffff;
+.container{
+    background: lightblue;
+    width:1080px;
+    height: auto;
+    margin-left: 255px;
 }
-
-h2{
+.container h1{
 font-family: IBM Plex Sans;
 font-style: normal;
 font-weight: normal;
-font-size: 30px;
+font-size: 40px;
 line-height: 36px;
-/* or 75% */
-text-align: center;
-color: #000000;
+
 }
 
-#hline1{
-position: absolute;
-width: 1078px;
-height: 0px;
-left: 260px;
-top: 150px;
-border: 1px solid #39D5D5;
+#form1{
+    height: 600px;
+    background: #ffffff;
 }
 
 #jobname{
   font-weight: bold;
   font-size: 20px;
   padding-top: 20px;
+  margin-left: -130px;
 }
 
 .form-control{
@@ -183,34 +156,66 @@ border: 1px solid #39D5D5;
     font-weight: bold;
     font-size: 20px;
     padding-top: 20px;
-    margin-left: -15px;
+    margin-left: 0px;
 }
 
-.btn{
-    width:150px;
-    height: 30px;
-    margin-top: -100px;
-    margin-left: 220px;
-    background: #f1f3f5;
-    color:#000000;
-     
+.menu{
+    margin-top: 25px;
+    padding-left: px;
+    display: flex;
 }
 
-.dropdown-menu{
-background: #ffffff;
+#hmenu1{
+    cursor: pointer;
+    font-size: 20px;
+    margin-left: 0px;
+    margin-top: px;
+    position: relative;
+}
+#hmenu2{
+    cursor: pointer;
+    font-size: 20px;
+    margin-left: 80px;
+    margin-top: px;
+    position: relative;
+    
+}
+#hmenu3{
+    cursor: pointer;
+    font-size: 20px;
+    margin-left: 80px;
+    margin-top: px;
+    position: relative;
+}
+#hmenu4{
+   cursor: pointer;
+    font-size: 20px;
+    margin-left: 80px;
+    margin-top: px;
+    position: relative;
 }
 
-.dropdown-item{
-    /* box-sizing: border-box;
-    background: #39D5D5; */
+#hmenu5{
+    cursor: pointer;
+    font-size: 20px;
+    margin-left: 80px;
+    margin-top: px;
+    position: relative;
 }
-
+    
+.card{
+    width: 1000px;
+   height: 250px;
+   margin-left: -260px;
+   margin-bottom: 20px
+}   
 
 .btn1{
     width:150px;
     height: 30px;
-    margin-top: 250px;
-    margin-left: 80px;
+    margin-top: 300px;
+    margin-left: 0px;
+    margin-bottom: 20px;
     background: #4EABE6;
     border: 1px solid #4EABE6;
     border-radius: 10px;
@@ -223,20 +228,57 @@ background: #ffffff;
     color: #FFFFFF;
 }
 
-.btn-toolbar{
-    padding-top: 80px;
-    padding-left: 200px;
-    float: center;
-
+.btn2{
+    width:150px;
+    height: 30px;
+    margin-top: 500px;
+    margin-left: 100px;
+    margin-bottom: 20px;
+    background: #4EABE6;
+    border: 1px solid #4EABE6;
+    border-radius: 10px;
+    font-family: IBM Plex Sans;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 20px;
+    text-align: center;
+    color: #FFFFFF;
 }
 
-.btn-group{
-   margin-left: -200px;
-
-   padding-left: 5px;
+.btn3{
+    width:150px;
+    height: 30px;
+    margin-top: 500px;
+    margin-left: 100px;
+    margin-bottom: 20px;
+    background: #4EABE6;
+    border: 1px solid #4EABE6;
+    border-radius: 10px;
+    font-family: IBM Plex Sans;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 20px;
+    text-align: center;
+    color: #FFFFFF;
 }
 
-.btn btn-secondary{
-    padding-top: 125px;
+.btn4{
+    width:150px;
+    height: 30px;
+    margin-top: 500px;
+    margin-left: 100px;
+    margin-bottom: 20px;
+    background: #4EABE6;
+    border: 1px solid #4EABE6;
+    border-radius: 10px;
+    font-family: IBM Plex Sans;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 20px;
+    text-align: center;
+    color: #FFFFFF;
 }
 </style>
